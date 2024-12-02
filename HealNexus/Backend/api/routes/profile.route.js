e3a3deb18dbca4395f1679d272fc02d8ccdc81df
Hitanshu Variya <hjvariya1@gmail.com>
2024-12-02 19:05:46 +0530

    Removing the TokenVerification middleware from creating profiles.

--- HealNexus/Backend/api/routes/profile.route.js
+++ HealNexus/Backend/api/routes/profile.route.js
@@ -18,19 +18,19 @@ import upload from '../../middleware/multer.js';
 
 const router = express.Router();
 
-router.post('/create-patient', verifyToken, upload.single('image'), createPatientProfile);
+router.post('/create-patient', upload.single('image'), createPatientProfile);
 router.put('/update-patient', verifyToken, upload.single('image'), updatePatientProfile);
 router.get('/get-patient', verifyToken, getPatientProfile);
 
-router.post('/create-doctor', verifyToken, upload.single('image'), createDoctorProfile);
+router.post('/create-doctor', upload.single('image'), createDoctorProfile);
 router.put('/update-doctor/:id', verifyToken, updateDoctorProfile);
 router.get('/get-doctor/:id', verifyToken, getDoctorProfile);
 
-router.post('/create-lab-technician', verifyToken, upload.single('image'), createLabTechnicianProfile);
+router.post('/create-lab-technician', upload.single('image'), createLabTechnicianProfile);
 router.put('/update-lab-technician/:id', verifyToken, updateLabTechnicianProfile);
 router.get('/get-lab-technician/:id', verifyToken, getLabTechnicianProfile);
 
-router.post('/create-pharmacist', verifyToken, upload.single('image'), createPharmacistProfile);
+router.post('/create-pharmacist', upload.single('image'), createPharmacistProfile);
 router.put('/update-pharmacist/:id', verifyToken, updatePharmacistProfile);
 router.get('/get-pharmacist/:id', verifyToken, getPharmacistProfile);
 
