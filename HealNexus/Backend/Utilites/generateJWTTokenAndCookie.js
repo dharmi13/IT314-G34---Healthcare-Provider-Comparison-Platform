7520d03c16bb75982608a6db9705093cdc51c837
chaudhariayush <chaudhariayush3200@gmail.com>
2024-12-02 11:37:21 +0530

    Deployment error

--- HealNexus/Backend/Utilites/generateJWTTokenAndCookie.js
+++ HealNexus/Backend/Utilites/generateJWTTokenAndCookie.js
@@ -10,7 +10,6 @@ const GenerateJWTTokenAndCookie = (userID, res) => {
     res.cookie("jwt", token, {
         maxAge: 10*24*60*60*1000,
         httpOnly: true,
-        sameSite: true,
         secure: process.env.MODE !== "development"
     })
 };
